b3493c7e0bb3f9b0c9bd8e71fd66f82bf537c8c4
moonymel <musumeci.me@gmail.com>
2024-01-10 15:11:33 +0100

    [update] setting style in AppMain.vue

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,11 +5,30 @@ export default {
 </script>
 
 <template lang="">
-    <div>
-        provaMain
-    </div>
+    <main class="py-3">
+        <div class="container my-cont">
+            <div class="row my-row">
+                <div class="col-12 col-md-10 col-lg-5">
+                    
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
-<style lang="">
-    
+<style lang="scss">
+@use '../styles/generals.scss' as *;
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variables' as *;
+
+main {
+    background-color: $my_orange;
+    height: 200px;
+
+    .my-cont {
+        background-color: white;
+        height: 100px;
+    }
+
+}
 </style>
