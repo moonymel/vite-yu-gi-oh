7d31fd2598566c7c5cd675e6407581bc58325b9c
moonymel <musumeci.me@gmail.com>
2024-01-10 14:53:41 +0100

    [update] completed header

--- src/App.vue
+++ src/App.vue
@@ -26,4 +26,6 @@ export default {
 
 <style lang="scss">
 @use './styles/generals.scss' as *;
+@use './styles/partials/mixins' as *;
+@use './styles/partials/variables' as *;
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,11 +5,26 @@ export default {
 </script>
 
 <template lang="">
-    <div>
-        provaaaa
-    </div>
+    <header>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/2/21/Yu-Gi-Oh%21.png"> <h1>Yu-Gi-Oh Api</h1>
+    </header>
 </template>
 
-<style lang="">
-    
+<style lang="scss">
+@use '../styles/generals.scss' as *;
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variables' as *;
+
+header {
+    padding: 20px;
+    background-color: white;
+    display: flex;
+    align-items: center;
+
+    img {
+        height: 20px;
+        vertical-align: middle;
+        margin-right: 20px;
+    }
+}
 </style>
