f3a916ba9d1650586b71230dcaddaac4015887ef
moonymel <musumeci.me@gmail.com>
2024-01-11 15:17:28 +0100

    [update] added apiArchetypes and AppSearch.vue

---
+++ src/components/AppSearch.vue
@@ -0,0 +1,25 @@
+<script>
+import { store } from '../store';
+
+export default {
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template lang="">
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+@use '../styles/generals.scss' as *;
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variables' as *;
+</style>
--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { reactive } from 'vue'
 
 export const store = reactive({
     endpoint: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
+    apiArchetypes: 'https://db.ygoprodeck.com/api/v7/archetypes.php',
     cardsList: [],
     loading: true,
 })
